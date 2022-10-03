7cef4321623ef4c942f1b38a4b78e4be1a092734
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-10-03 10:01:31 -0500

    warm up 10-3

--- js/Exercises.js
+++ js/Exercises.js
@@ -30,14 +30,14 @@
 // example input: [1,2,3,4,5] expected output: [2,4]
 
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-function evenNum(x){
-    if x == x % 2 === 0
-        return x
-}
-
-console.log(evenNum(numbers));
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+// function evenNum(x){
+//     if x == x % 2 === 0
+//         return x
+// }
+//
+// console.log(evenNum(numbers));
 
 
 //Write a function that takes an array of objects and a string as arguments. Add a property with key ‘continent’ and value equal to the string to each of the objects. Return the new array of objects. Don’t mutate the original array.
@@ -45,3 +45,22 @@ console.log(evenNum(numbers));
 // expected output: [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
 // (edited)
 
+// example input: [{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'
+// expected output: [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
+//
+// function x (con)
+
+
+// re-write the following using a for loop: var i = 20; while (i >= 0) { console.log(i); i--; }
+
+for (let i = 20; i >= 0; i--){
+    console.log(i);
+}
+
+// Add ‘strawberry’ to the beginning, middle, and end of the array. var fruits = ["mango", "blueberries", "oranges", "banana", "papaya", "kiwi"];
+
+let fruits = ["mango", "blueberries", "oranges", "banana", "papaya", "kiwi"]
+
+fruits = ["strawberry", ...fruits.slice(0,4), "strawberry", ...fruits.slice(3),"strawberry"]
+
+console.log(fruits)
