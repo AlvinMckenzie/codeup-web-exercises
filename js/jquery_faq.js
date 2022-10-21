ccc9cd577f472613e68b82fb2321ee971f752d37
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-10-21 12:06:34 -0500

    jQuery Traversing Methods ex. complete

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,8 +1,24 @@
 "use strict";
 
 $('dt').click(function (e) {
-
     $('dd').toggleClass('invisible');
+    $('dd').removeClass('highlighted')
     $(this).next('dd').toggleClass('highlighted')
-
 });
+
+$('.factList').click(function () {
+
+    $('h3').each(function(x, y) {
+
+        $(y).next().children().last().css('background-color', 'yellow')
+    })
+})
+
+$('h3').click(function () {
+    $(this).next().children().css('font-weight', 'bold')
+})
+
+$('li').click(function () {
+    $(this).parent().children().first().css('color', 'blue')
+
+})
