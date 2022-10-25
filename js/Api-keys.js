51b72778d110cd45753f5f8663136b07f5b76b35
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-10-25 18:34:51 -0500

    weather map complete

---
+++ js/Api-keys.js
@@ -0,0 +1,6 @@
+// mapbox key
+const mapToken = 'pk.eyJ1IjoiYWx2aW5tY2tlbnppZSIsImEiOiJjbDluODV3MnMwMDBzM3lxdWN1Z2lld3pvIn0.ju-g_lwq5unZe1KE-O5gJA'
+
+//weather Map
+const weatherToken = '4f6e293822acbfeedf9533514416ec0d'
+
